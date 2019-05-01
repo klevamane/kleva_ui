bb2eafbfe999187e3bf1fbb9162f4b837a3ef0bf
Onengiye Richard <klevamane@gmail.com>
2019-05-01 15:26:14 +0100

    add technologies section and task section

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+// myelement = document.getElementById(edit_tooltip);
+
+// myelement = function() {
+//     ('[data-toggle="tooltip"]').tooltip();
+// }
